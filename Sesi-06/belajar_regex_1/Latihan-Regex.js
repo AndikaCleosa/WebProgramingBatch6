c2a3fa659958280b0ae8bf274ea0a7df86044c65
AndikaCleosa <andika.dwi93.ad@gmail.com>
2023-06-07 16:30:15 +0700

    Saya update materi sesi 3

---
+++ Sesi-06/belajar_regex_1/Latihan-Regex.js
@@ -0,0 +1,6 @@
+var foo = "Belajar Javascript";
+var pola = /Javascript/;
+
+console.log(pola.test(foo)); // true
+console.log(/Belajar/.test(foo)); // true
+console.log(/Belajar/.test(foo)); // false
